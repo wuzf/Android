842284bb422dc9f3be51c5ba1bc347ca5acd8ea7
wuzhifeng <zhifeng_wu@netcom-hk.com>
2012-08-30 13:33:31 +0800

    增加android安装包签名工具，直接运行sign_all即可自动签名所有包，修改一下内容即可签名所有的apk

---
+++ sign/sign_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#设定要签名的文件后缀，如zip或apk
+filetype=zip
+#找出当前目录下（不包含子文件夹）未签名过的并且以filetype为后缀的文件
+find . -maxdepth 1 -name "*.$filetype" -and -not -name "*_signed.$filetype"| while read fname
+do
+  echo "signing $fname";
+  #签名
+  java -Xmx2048m -jar signapk.jar -w testkey.x509.pem testkey.pk8 $fname ${fname%.$filetype}_signed.$filetype
+  echo "$fname was signed and rename to ${fname%.$filetype}_signed.$filetype"
+  #判断签名后的文件是否存在，若存在说明签名成功删除源文件，如内存不够等原因有可能存在签名不成功则不删除源文件。
+  if   [ -e ${fname%.$filetype}_signed.$filetype ] 
+  then 
+    rm $fname
+  fi
+done
+echo "All $filetype files were Signed Complete!"
