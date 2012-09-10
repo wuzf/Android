a3a72d644bdb46e3722368fa12bbb43c621a7f1c
wuzhifeng <zhifeng_wu@netcom-hk.com>
2012-09-10 15:19:34 +0800

    更新使用方法。
    unpack脚本用法：
    ./unpack.sh recovery.img
    ./unpack.sh boot.img
    
    repack脚本用法：
    ./repack.sh recovery.img
    ./repack.sh boot.img

--- unpack repack kernel/LGLU6200/repack.sh
+++ unpack repack kernel/LGLU6200/repack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./mkbootfs ./ramdisk | gzip > ramdisk-new.gz
-./mkbootimg --kernel $1.img-kernel --ramdisk ramdisk-new.gz --cmdline "vmalloc=450M, console=ttyDCC0,115200,n8 androidboot.hardware=qcom" --base 0x40200000 --pagesize 2048 --ramdiskaddr 0x41300000 -o new$1.img
+./mkbootimg --kernel $1-kernel --ramdisk ramdisk-new.gz --cmdline "vmalloc=450M, console=ttyDCC0,115200,n8 androidboot.hardware=qcom" --base 0x40200000 --pagesize 2048 --ramdiskaddr 0x41300000 -o new$1
 
