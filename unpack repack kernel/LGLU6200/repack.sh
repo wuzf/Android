143de49c254c86599f3c11f50345b5329622f5f8
wuzhifeng <zhifeng_wu@netcom-hk.com>
2012-09-13 17:33:44 +0800

    更新ramdiskaddr，ICS好像是0x41A00000，GB好像是0x41300000，若设置错误不知道会怎样，还没试过

--- unpack repack kernel/LGLU6200/repack.sh
+++ unpack repack kernel/LGLU6200/repack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ./mkbootfs ./ramdisk | gzip > ramdisk-new.gz
-./mkbootimg --kernel $1-kernel --ramdisk ramdisk-new.gz --cmdline "vmalloc=450M, console=ttyDCC0,115200,n8 androidboot.hardware=qcom" --base 0x40200000 --pagesize 2048 --ramdiskaddr 0x41300000 -o new$1
+#GB的ramdiskaddr好像是0x41300000，ICS的ramdiskaddr好像是0x41A00000，若设置错误不知道会怎样，没试过
+./mkbootimg --kernel $1-kernel --ramdisk ramdisk-new.gz --cmdline "vmalloc=450M,console=ttyDCC0,115200,n8 androidboot.hardware=qcom" --base 0x40200000 --pagesize 2048 --ramdiskaddr 0x41A00000 -o new$1
 
